03d884c9b53a32f87786cd2ab952b0a7abdd4411
Viktor Ens <v.ens86@yahoo.de>
2024-04-18 16:10:31 +0200

    add fibonaci algorithm

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(calculateFibonacci(10));
+    }
+
+    public static int calculateFibonacci(int n) {
+
+        if (n <= 1) {
+            return n;
+        } else {
+            int fib1 = 1;
+            int fib2 = 0;
+            int sum = 0;
+
+            for(int i = 2; i <= n; i++) {
+
+                sum = fib1 + fib2;
+                fib2 = fib1;
+                fib1 = sum;
+            }
+            return sum;
+        }
+
     }
 }
--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -1,4 +1,46 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MainTest {
+
+    @Test
+    void whenNisOne_ResultIs1() {
+        //Given
+        int n = 1;
+
+        //When
+        int actual = Main.calculateFibonacci(n);
+
+        //Then
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
+    @Test
+    void whenNisZero_ResultIsZero() {
+        //Given
+        int n = 0;
+
+        //When
+        int actual = Main.calculateFibonacci(n);
+
+        //Then
+        int expected = 0;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void whenNisSeven_ResultIs13() {
+        //Given
+        int n = 7;
+
+        //When
+        int actual = Main.calculateFibonacci(n);
+
+        //Then
+        int expected = 13;
+        assertEquals(expected, actual);
+    }
 }
